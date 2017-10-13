f11e87da5761193bd0f6569ea340a817c5575b46
ebergasa <ebergasa@users.noreply.github.com>
2017-10-13 14:27:17 +0200

    Including UTF-8 encoding header in HTTP response

--- controllers/freecite_controller.rb
+++ controllers/freecite_controller.rb
@@ -3,7 +3,7 @@ require 'json'
 
 class FreeciteController < Sinatra::Base
   get '/search' do
-    content_type 'application/json'
+    content_type 'application/json ; charset=utf-8'
 
     query = params["q"] || ""
     JSON.generate(CRFParser.new.parse_string(query) || {})
